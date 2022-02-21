e69100b5686412bd3ca586bddf24859f6e9e2483
jean <thomas.jean.lynch@gmail.com>
2022-02-21 17:30:56 -0500

    Add code for collapse-resources function

--- src/collapse-resources.js
+++ src/collapse-resources.js
@@ -1,4 +1,15 @@
 javascript: (function () {
-  /* TODO: need some way to defer this until the relevant markup has loaded */
-  console.log("TODO");
+  /* FIXME: can we make this list customizeable? (maybe once dynamic value replacement is implemented for templates) */
+  /* TODO: need some way to defer this until the relevant markup has loaded (MutationObserver?) */
+  /* TODO: extend this bookmarklet to also click on specific pins to load a preset? */
+  const collapse = [
+    'Waypoints', 'Guide',
+    'Enemies (Common)', 'Enemies (Elite)', 'Enemies (Boss)', 'Enemies (Easter Egg)',
+    'Local Specialties', 'Animals', 'Materials', 'Fishing', 'Ores', 'Wood',
+  ];
+  document.querySelectorAll('.layer-control__group:not(.layer-control__group--folded)')
+    .forEach(section => {
+      const title = section.querySelector('.layer-control__group-title-name');
+      if (collapse.includes(title.innerText)) title.click();
+    })
 })();
