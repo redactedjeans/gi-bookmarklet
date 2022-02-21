1fbc678c19f9795e75f2a569a2578a8b99e2111f
jean <thomas.jean.lynch@gmail.com>
2022-02-21 18:41:02 -0500

    Facilitate concatenation of multiple fns into a single bookmarklet

--- src/collapse-resources/main.js
+++ src/collapse-resources/main.js
@@ -1,15 +1,13 @@
-javascript: (function () {
-  /* FIXME: can we make this list customizeable? (maybe once dynamic value replacement is implemented for templates) */
-  /* TODO: need some way to defer this until the relevant markup has loaded (MutationObserver?) */
-  /* TODO: extend this bookmarklet to also click on specific pins to load a preset? */
-  const collapse = [
-    'Waypoints', 'Guide',
-    'Enemies (Common)', 'Enemies (Elite)', 'Enemies (Boss)', 'Enemies (Easter Egg)',
-    'Local Specialties', 'Animals', 'Materials', 'Fishing', 'Ores', 'Wood',
-  ];
-  document.querySelectorAll('.layer-control__group:not(.layer-control__group--folded)')
-    .forEach(section => {
-      const title = section.querySelector('.layer-control__group-title-name');
-      if (collapse.includes(title.innerText)) title.click();
-    })
-})();
+/* FIXME: can we make this list customizeable? (maybe once dynamic value replacement is implemented for templates) */
+/* TODO: need some way to defer this until the relevant markup has loaded (MutationObserver?) */
+/* TODO: extend this bookmarklet to also click on specific pins to load a preset? */
+const collapse = [
+  'Waypoints', 'Guide',
+  'Enemies (Common)', 'Enemies (Elite)', 'Enemies (Boss)', 'Enemies (Easter Egg)',
+  'Local Specialties', 'Animals', 'Materials', 'Fishing', 'Ores', 'Wood',
+];
+document.querySelectorAll('.layer-control__group:not(.layer-control__group--folded)')
+  .forEach(section => {
+    const title = section.querySelector('.layer-control__group-title-name');
+    if (collapse.includes(title.innerText)) title.click();
+  });
--- src/compact-sidebar/main.js
+++ src/compact-sidebar/main.js
@@ -1,14 +1,12 @@
-javascript: (function () {
-  /* TODO: add option to inline CSS so it can't be changed remotely? */
-  const id = 'cscss'; /* Compact Sidebar CSS */
-  const url = '{url}/src/compact-sidebar/styles.css';
-  let link = document.getElementById(id);
-  if (link === null) {
-    link = document.createElement('link');
-    link.id = id;
-    link.rel = 'stylesheet';
-    document.getElementsByTagName('head')[0].appendChild(link);
-  }
-  /* add date for cache-busting */
-  link.href = `${url}?v=${Date.now()}`;
-})();
+/* TODO: add option to inline CSS so it can't be changed remotely? */
+const id = 'cscss'; /* Compact Sidebar CSS */
+const url = '{url}/src/compact-sidebar/styles.css';
+let link = document.getElementById(id);
+if (link === null) {
+  link = document.createElement('link');
+  link.id = id;
+  link.rel = 'stylesheet';
+  document.getElementsByTagName('head')[0].appendChild(link);
+}
+/* add date for cache-busting */
+link.href = `${url}?v=${Date.now()}`;
